6128dea20b2fb97c57ed8ed7e5e9b95430a89407
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-04-12 11:08:45 +0100

    Add initial setup

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '2.0.0'
+
+require 'bundler'
+
+gem 'pry'
+gem 'require_all'
+gem 'rspec'
---
+++ start.rb
@@ -0,0 +1,7 @@
+#!/usr/bin/env ruby
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __FILE__)
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+Bundler.require(:default)
+
+require_all 'lib'
