344b14a98514cfcd7d95d40dae94495db6da0ca5
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-04-15 21:50:01 +0100

    Add image#clear_or_create_bitmap spec

--- spec/image_spec.rb
+++ spec/image_spec.rb
@@ -16,8 +16,7 @@ describe Image do
   describe "#initialize" do
 
     context "When image size less or equal 250" do
-
-      it "assigns width, height and" do
+      it "assigns width, height and bitmap" do
         expect(image.width).to eq 2
         expect(image.height).to eq 3
         expect(image.bitmap).to eq new_bitmap
@@ -25,7 +24,6 @@ describe Image do
     end
 
     context "When image size has exceeded" do
-
       it "raises BitmapSizeError" do
         expect { Image.new(251, 1) }.to raise_exception(BitmapSizeError)
         expect { Image.new(1, 251) }.to raise_exception(BitmapSizeError)
@@ -33,6 +31,17 @@ describe Image do
         expect { Image.new(0, 1) }.to raise_exception(BitmapSizeError)
       end
     end
+
   end
 
+  describe "#clear_or_create_bitmap" do
+    before do
+      image.instance_variable_set(:@bitmap, edited_bitmap)
+      image.clear_or_create_bitmap
+    end
+
+    it "replaces edited bitmap with new one" do
+      expect(image.bitmap).to eq new_bitmap
+    end
+  end
 end
